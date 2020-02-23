e22ba5568629ea7244fc8c0537f50689188f43dd
Nicolás Cerrini <ceneon@gmail.com>
2020-02-23 17:13:05 -0300

    Updated tasks.rb to set a local `dotenv_location`
    As seen at https://github.com/dostro/mina-dotenv/

--- lib/mina/dotenv/tasks.rb
+++ lib/mina/dotenv/tasks.rb
@@ -2,6 +2,7 @@ require 'mina/dotenv/utils'
 require 'mina/scp'
 
 namespace :dotenv do
+  dotenv_location = ".env"
   desc 'Copies the local .env file to the server'
   task :push do
     dotenv = Mina::Dotenv::Utils.read_file(fetch(:dotenv_location))
